bc58ddbf55f403ef1f9e36aba20443a4f2905bb8
David Ferrero <dferre@elpuig.xeill.net>
2021-04-29 17:55:15 +0200

    pequeños retoques en los hints

--- app/src/main/java/com/example/futinfov2/SBCFragment.java
+++ app/src/main/java/com/example/futinfov2/SBCFragment.java
@@ -2,63 +2,39 @@ package com.example.futinfov2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SBCFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.bumptech.glide.Glide;
+import com.example.futinfov2.databinding.FragmentCrearTuCartaBinding;
+import com.example.futinfov2.databinding.FragmentSBCBinding;
+import com.google.firebase.storage.FirebaseStorage;
+
+
 public class SBCFragment extends Fragment {
+    private FragmentSBCBinding binding;
+    private NavController navController;
+    private FirebaseStorage firebaseStorage;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SBCFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SBCFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SBCFragment newInstance(String param1, String param2) {
-        SBCFragment fragment = new SBCFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return (binding = FragmentSBCBinding.inflate(inflater, container, false)).getRoot();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_s_b_c, container, false);
+        /*Glide.with(requireContext())
+                .load("https://firebasestorage.googleapis.com/v0/b/futinfo-1b129.appspot.com/o/iconos_app%2FproLeague.png?alt=media&token=3e6177f8-1c92-4360-9986-795c77676d4e")
+                .into(binding.img);
+                */
+
     }
 }
