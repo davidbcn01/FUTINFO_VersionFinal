2ca58a45b1aeeb05acdebb4a49f64dc3f750236e
davidbcn01 <dferre@elpuig.xeill.net>
2021-05-03 01:59:15 +0200

    avances en la bbdd

--- app/src/main/java/com/example/futinfov2/InicioFragment.java
+++ app/src/main/java/com/example/futinfov2/InicioFragment.java
@@ -13,7 +13,9 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.bumptech.glide.Glide;
 import com.example.futinfov2.databinding.FragmentInicioBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.thekhaeng.pushdownanim.PushDownAnim;
 
 import static com.thekhaeng.pushdownanim.PushDownAnim.MODE_SCALE;
@@ -22,6 +24,7 @@ public class InicioFragment extends Fragment {
 
     FragmentInicioBinding binding;
     private NavController navController;
+    private FirebaseFirestore firebaseFirestore;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return (binding = FragmentInicioBinding.inflate(inflater, container, false)).getRoot();
@@ -30,6 +33,34 @@ public class InicioFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         navController = Navigation.findNavController(view);
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseFirestore.collection("jugadores").document("iUZSJOddr0oP8hL2NJJp").addSnapshotListener(((value, error) ->{
+            Jugador jugador = new Jugador(value.getString("name"),value.getString("position"), Math.toIntExact(value.getLong("rating")),value.getString("country"),value.getString("team"),value.getString("face"),value.getString("card"));
+            binding.position2.setText(jugador.posicion);
+            binding.name.setText(jugador.nombre);
+            binding.rating.setText(String.valueOf(jugador.media));
+            Glide.with(requireContext())
+                    .load(jugador.cara)
+                    .into(binding.face);
+            Glide.with(requireContext())
+                    .load(jugador.pais)
+                    .into(binding.country);
+            Glide.with(requireContext())
+                    .load(jugador.equipo)
+                    .into(binding.team);
+            Glide.with(requireContext())
+                    .load(jugador.tipo)
+                    .into(binding.tipo);
+
+        }
+
+                ));
+
+
+
+        Glide.with(requireContext())
+                .load("https://firebasestorage.googleapis.com/v0/b/futinfo-1b129.appspot.com/o/iconos_app%2FxpIcon.png?alt=media&token=cc6a994b-65e8-45dd-bfb3-7fea549374d3")
+                .into(binding.imageView3);
 
 
 
